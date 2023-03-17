365a00a71ad99783d042d4abbd24e1d61ff44b16
E112222 <127614694+E112222@users.noreply.github.com>
2023-03-17 01:20:01 +0100

    Initial commit

---
+++ PANO360/Cloitre/datal.js
@@ -0,0 +1,185 @@
+var APP_DATA = {
+    "scenes": [
+        {
+            "id": "2-cloitrelourde",
+            "name": "CloitreLourde",
+            "levels": [
+              {
+                "tileSize": 256,
+                "size": 256,
+                "fallbackOnly": true
+              },
+              {
+                "tileSize": 512,
+                "size": 512
+              },
+              {
+                "tileSize": 512,
+                "size": 1024
+              },
+              {
+                "tileSize": 512,
+                "size": 2048
+              },
+              {
+                "tileSize": 512,
+                "size": 4096
+              }
+            ],
+            "faceSize": 2944,
+            "initialViewParameters": {
+              "yaw": -0.6616465244179164,
+              "pitch": 0.056820383649569095,
+              "fov": 1.463665799827691
+            },
+            "linkHotspots": [
+              {
+                "yaw": 0.9248184718710597,
+                "pitch": 0.5677199155987687,
+                "rotation": 0,
+                "target": "0-cloitre"
+              }
+            ],
+            "infoHotspots": [
+              {
+                "yaw": 1.8888024591151238,
+                "pitch": 0.30242193627141845,
+                "title": "Lourdes",
+                "text": "<div  <a href='' ><img src='./img/ThuysseAlt.png'  ></div>"
+              },
+              {
+                "yaw": -1.4701109174131055,
+                "pitch": 0.13967914766048573,
+                "title": "Les Zi-Stoire",
+                "text": "Ekip"
+              }
+            ]
+          },    
+    {
+        "id": "1-cloitreclocheton",
+        "name": "CloitreClocheton",
+        "levels": [
+          {
+            "tileSize": 256,
+            "size": 256,
+            "fallbackOnly": true
+          },
+          {
+            "tileSize": 512,
+            "size": 512
+          },
+          {
+            "tileSize": 512,
+            "size": 1024
+          },
+          {
+            "tileSize": 512,
+            "size": 2048
+          },
+          {
+            "tileSize": 512,
+            "size": 4096
+          }
+        ],
+        "faceSize": 2944,
+        "initialViewParameters": {
+          "yaw": 0.9585885276338022,
+          "pitch": 0.08815876713200232,
+          "fov": 1.463665799827691
+        },
+        "linkHotspots": [
+          {
+            "yaw": 0.9248184718710597,
+            "pitch": 0.5677199155987687,
+            "rotation": 0,
+            "target": "0-cloitre"
+          }
+        ],
+        "infoHotspots": [
+          {
+            "yaw": 1.6684512403019838,
+            "pitch": 0.1555408192745773,
+            "title": "Rico",
+            "text": "Current's 70-170 AN 222"
+          }
+        ]
+      },
+      {
+        "id": "0-cloitre",
+        "name": "Cloitre",
+        "levels": [
+          {
+            "tileSize": 256,
+            "size": 256,
+            "fallbackOnly": true
+          },
+          {
+            "tileSize": 512,
+            "size": 512
+          },
+          {
+            "tileSize": 512,
+            "size": 1024
+          },
+          {
+            "tileSize": 512,
+            "size": 2048
+          },
+          {
+            "tileSize": 512,
+            "size": 4096
+          }
+        ],
+        "faceSize": 3456,
+        "initialViewParameters": {
+          "yaw": 0.03817698481870835,
+          "pitch": -0.08386029230378611,
+          "fov": 1.463665799827691
+        },
+        "linkHotspots": [
+          {
+            "yaw": 0.767349991195033,
+            "pitch": 0.38368338086258547,
+            "rotation": 6.283185307179586,
+            "target": "1-cloitreclocheton"
+          },
+          {
+            "yaw": -2.3297121010601316,
+            "pitch": 0.39648825290056067,
+            "rotation": 0,
+            "target": "2-cloitrelourde"
+          }
+        ],
+        "infoHotspots": [
+          {
+            "yaw": 0.10036136911062599,
+            "pitch": 0.33903270880067105,
+            "title": "Rico",
+            "text": "Current's 70-170 AN 222"
+          },
+          {
+            "yaw": 0.5965947173990891,
+            "pitch": -0.46382144198510034,
+            "title": "Clocheton",
+            "text": "<div  <a href='' ><img src='./img/ThuysseClocheton.png'  ></div>"
+          },
+          {
+            "yaw": -3.0945363851999037,
+            "pitch": 0.27868188118821813,
+            "title": "TT",
+            "text": "<div  <a href='' ><img src='./img/ThuysseTT.png'  ></div>"
+          }
+        ]
+      },
+      
+
+    ],
+    "name": "Project Title",
+    "settings": {
+      "mouseViewMode": "drag",
+      "autorotateEnabled": false,
+      "fullscreenButton": false,
+      "viewControlButtons": false
+    }
+  };
+  
---
+++ README.md
@@ -0,0 +1,7 @@
+# BH-master
+BoquetteHistoireV0
+Vinit 
+Undesign for mobile --> ccs(fixed elements)
+Use of Marzipano iframe hosted on Git to RV
+
+Still On dev
